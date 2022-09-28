3a363c97399d71dd4ce0fa586d1f8db97e5873f0
PythonFR_student <113584205+ConcurrenceKastouille@users.noreply.github.com>
2022-09-28 08:25:16 +0200

    Update morpion.py

--- morpion.py
+++ morpion.py
@@ -90,7 +90,7 @@ def morpion():
         if ligne1[2]=="V" and ligne2[1]=="V" and ligne3[0]=="V":
             return "Le joueur 2 est le gagnant, Bravo !"
         
-                #On vérifie s'il n'y a pas match nul
+        #On vérifie s'il n'y a pas match nul
 
         if not "O" in Jeu:
             return "Pas de gagnants, match nul"
